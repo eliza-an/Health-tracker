65cf46ba19ea11ae31dfec07ea48819cebcf49d1
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-06-19 16:28:34 +0100

    added text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@
 import './App.css';
 
 function App() {
+  return(
+  <div>
+
+  <h2>Nothing changes if nothing changes.</h2>
+  <h1>Take the next step.</h1>
+  <h3>The most effective way to change your habits is to focus not on what you wish to achieve, but who you wish to become. Your identity emerges out of your habits. Every action is a vote for the type of person you wish to become 
+~ James Clear</h3>
+  </div> 
+)
 }
 
 export default App;

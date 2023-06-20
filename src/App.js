37bb2ef53a50e24336a1de5aafa0118b15b5a8e9
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-06-20 23:48:35 +0100

    added image

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <h3>The most effective way to change your habits is to focus not on what you wish to achieve, but who you wish to become. Your identity emerges out of your habits. Every action is a vote for the type of person you wish to become 
         ~ James Clear
       </h3>
+      <img className="firstImage"src='first.jpg' alt=' a person at a computer. Only hands with a mug and some notebooks'></img>
   </div>
 </div> 
 )

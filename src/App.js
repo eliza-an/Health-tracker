17a0534bb6c2ab7ab5154504fa900b1ae692e33b
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-06-21 12:03:49 +0100

    finished front page#

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 
 function App() {
   return(
-  <div className='container'>
-    <div className='text'>
-      <h2>Nothing changes if nothing changes.</h2>
-      <h1>Take the next step.</h1>
-      <h3>The most effective way to change your habits is to focus not on what you wish to achieve, but who you wish to become. Your identity emerges out of your habits. Every action is a vote for the type of person you wish to become 
-        ~ James Clear
-      </h3>
+  <div>
+    <div className='container'>
+      <div className='text'>
+        <h2>Nothing changes if nothing changes.</h2>
+        <h1>Take the next step.</h1>
+        <h3>The most effective way to change your habits is to focus not on what you wish to achieve, but who you wish to become. Your identity emerges out of your habits. Every action is a vote for the type of person you wish to become 
+         <br></br>
+          ~ James Clear
+        </h3>
+      </div> 
       <div className='image'>
-      <img className="firstImage"src='first.jpg' alt=' a person at a computer. Only hands with a mug and some notebooks'></img></div>
+        <img className="firstImage"src='first.jpg' alt=' a person at a computer. Only hands with a mug and some notebooks'></img>
+      </div>
   </div>
-</div> 
+    
+  </div>
 )
 }
 

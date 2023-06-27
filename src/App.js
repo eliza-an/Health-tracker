94e9f02ff120f3519441649918546ec8e2d5f7c3
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-06-27 10:33:23 +0100

    okay NOW i fixed the issue

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import BoxTracker from "./pages/tracker";
+
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/track" element={<BoxTracker />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,17 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
-import BoxTracker from "./pages/tracker";
-import React, { Component } from 'react';
 
-export default class App extends Component() {
-  render(){
-  return (
-    <Router>
-      <div>
-        <h2>Welcome to React Router Tutorial</h2>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li>
-              <Link to={"/"} className="nav-link">
-                {" "}
-                Home{" "}
-              </Link>
-            </li>
-            <li>
-              <Link to={"/tracker"} className="nav-link">
-                tracker
-              </Link>
-            </li>
-            {/* <li>
-              <Link to={"/about"} className="nav-link">
-                About
-              </Link>
-            </li> */}
-          </ul>
-        </nav>
-        <hr />
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/tracker" component={BoxTracker} />
-          {/* <Route path="/about" component={About} /> */}
-        </Routes>
-      </div>
-    </Router>
-  );}
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

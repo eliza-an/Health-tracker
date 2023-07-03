80dae8a7c4b4cebba2ed7310f749909d406cfafa
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-07-03 17:21:16 +0100

    UGH

--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -1,50 +1,64 @@
-import React, { useState} from "react";
-import "./tracker.css"
+import React, { useState } from "react";
+import "./tracker.css";
 import DateTime from "../components/dateTime";
 
-
-
-
 function BoxTracker() {
+  const [rowCount, setRowCount] = useState(1);
   const [columnCount, setColumnCount] = useState(31);
-  const [myColor, setMyColor] = useState(Array(columnCount).fill("white"));
-  const [title, setTitle] = useState("Add a title");
+  const [myColor, setMyColor] = useState([Array(columnCount).fill("white")]);
+  const [myTitles, setMyTitles] = useState([Array(rowCount).fill("")]);
 
-  const addColumn = () => {
-    setColumnCount(columnCount + 31);
-     setMyColor([...myColor, ...Array(31).fill("white")]);
-     
-
+  const prependInputBox = (rowIndex) => {
+    return (
+      <input
+        type="text"
+        name={`HabitName-${rowIndex}`}
+        value={myTitles[rowIndex]}
+        onChange={(e) => handleTitleChange(e, rowIndex)}
+      />
+    );
   };
 
+  const handleTitleChange = (e, rowIndex) => {
+    const updatedTitles = [...myTitles];
+    updatedTitles[rowIndex] = e.target.value;
+    setMyTitles(updatedTitles);
+  };
 
+  const addRow = () => {
+    setRowCount(rowCount + 1);
+    setMyColor([...myColor, Array(columnCount).fill("white")]);
+    setMyTitles([...myTitles, ""]);
+  };
 
-
-const handleClick = (index) => {
-  const updatedColors = [...myColor];
-  updatedColors[index] = updatedColors[index] === "white" ? "green" : "white";
-  setMyColor(updatedColors);
-};
+  const handleClick = (rowIndex, columnIndex) => {
+    const updatedColors = [...myColor];
+    updatedColors[rowIndex][columnIndex] = updatedColors[rowIndex][columnIndex] === "white" ? "green" : "white";
+    setMyColor(updatedColors);
+  };
 
   return (
     <div>
       <DateTime />
       <div className="gridContainer">
-        {/* creates a new array with length collumn count. ... spreads the aray into individual elements. .map executes a callback function over each element in the array */}
-        <div className="rowTitle" onClick={() => setTitle(<input type="text" name="HabitName" />)}> {title}</div>
-        {myColor.map((color, index) => (
-          <div
-            className="gridItem"
-            onClick={() => handleClick(index)}
-            style={{ backgroundColor: color }}
-            key={index}
-          >
-            yeet
+        {myColor.map((row, rowIndex) => (
+          <div key={`row-${rowIndex}`} className="gridRow">
+            {prependInputBox(rowIndex)}
+            {row.map((color, columnIndex) => (
+              <div
+                key={`cell-${rowIndex}-${columnIndex}`}
+                className="gridItem"
+                onClick={() => handleClick(rowIndex, columnIndex)}
+                style={{ backgroundColor: color }}
+              >
+                yeet
+              </div>
+            ))}
           </div>
         ))}
       </div>
-      <button className="Additional-Row" onClick={addColumn}>
-        Add a New Column
+      <button className="Additional-Row" onClick={addRow}>
+        Add a New Row
       </button>
     </div>
   );

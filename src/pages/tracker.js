e6bc40a71f33b989b30f8f427bb71f2bc88c5c54
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-07-03 15:48:27 +0100

    added a title div

--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -6,12 +6,15 @@ import DateTime from "../components/dateTime";
 
 
 function BoxTracker() {
-  const [columnCount, setColumnCount] = useState(32);
+  const [columnCount, setColumnCount] = useState(31);
   const [myColor, setMyColor] = useState(Array(columnCount).fill("white"));
+  const [title, setTitle] = useState("Add a title");
 
   const addColumn = () => {
-    setColumnCount(columnCount + 32);
-     setMyColor([...myColor, ...Array(32).fill("white")]);
+    setColumnCount(columnCount + 31);
+     setMyColor([...myColor, ...Array(31).fill("white")]);
+     setTitle(["Add a title"])
+
   };
 
 
@@ -27,14 +30,13 @@ const handleClick = (index) => {
     <div>
       <DateTime />
       <div className="gridContainer">
-        
         {/* creates a new array with length collumn count. ... spreads the aray into individual elements. .map executes a callback function over each element in the array */}
-        
-           {myColor.map((color, index) => (
+        <div className="rowTitle" onClick={() => setTitle("New title")}> {title}</div>
+        {myColor.map((color, index) => (
           <div
             className="gridItem"
             onClick={() => handleClick(index)}
-          style={{ backgroundColor: color }}
+            style={{ backgroundColor: color }}
             key={index}
           >
             yeet

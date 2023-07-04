b7a1455e1069f99619b3f04139c63eea9cdd080f
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-07-04 16:15:39 +0100

    okay getting there by adding some css

--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -7,29 +7,23 @@ import DateTime from "../components/datePicker/dateTime";
 
 
 function BoxTracker() {
-  const [columnCount, setColumnCount] = useState(daysInMonth());
-  const [myColor, setMyColor] = useState(Array(columnCount).fill("white"));
-
   // Get the current month and number of days
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth();
   const currentYear = currentDate.getFullYear();
-  const numDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const numDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); 
+  
+  
+  const [columnCount, setColumnCount] = useState(numDaysInMonth);
+  const [myColor, setMyColor] = useState(Array(columnCount).fill("white"));
+
+ 
 
   const [rowCount, setRowCount] = useState(1);
 
   const handleAddRow = () => {
     setRowCount(rowCount + 1);
-  };
-
-
-    function daysInMonth() {
-      const now = new Date();
-      return new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-    }
-
-  const addColumn = () => {
-    setColumnCount(columnCount + daysInMonth());
+    setColumnCount(numDaysInMonth);
     setMyColor([...myColor, ...Array(31).fill("white")]);
   };
 
@@ -51,7 +45,7 @@ function BoxTracker() {
       <table>
         <thead>
           <tr>
-            <th>Input</th>
+            <th className="habitName">Habit</th>
             {[...Array(numDaysInMonth)].map((_, index) => (
               <th key={index}>{index + 1}</th>
             ))}
@@ -59,12 +53,12 @@ function BoxTracker() {
         </thead>
         <tbody>
           {[...Array(rowCount)].map((_, rowIndex) => (
-            <tr key={rowIndex}>
+            <tr    key={rowIndex}>
               <td>
-                <input type="text" />
+               <input type="text" />
               </td>
               {[...Array(numDaysInMonth)].map((_, dayIndex) => (
-                <td key={dayIndex}>Cell</td>
+                <td className="gridItem"onClick={handleClick} key={dayIndex}></td>
               ))}
             </tr>
           ))}
